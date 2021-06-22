d6587510cb8d5834aa628f5ed74b8d34dadab6e0
wmytql <67404763+wmytql@users.noreply.github.com>
2021-06-22 17:03:24 +0800

    Update OrderService.java

--- src/com/itstudy/serive/OrderService.java
+++ src/com/itstudy/serive/OrderService.java
@@ -3,12 +3,6 @@ package com.itstudy.serive;
 import com.itstudy.bean.Cart;
 import com.itstudy.bean.User;
 
-/**
- * Author: LUOJIAN
- * Date: 2021/4/23
- * Time: 18:47
- * Decription:
- */
 public interface OrderService {
 
     /**
